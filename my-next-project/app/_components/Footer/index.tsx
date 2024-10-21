19585c14bddfd4bab29eba2be35402c5f4f3c523
kashihiro <jyasu0711hito@gmail.com>
2024-10-21 20:43:10 +0900

    10/21変更

---
+++ my-next-project/app/_components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+export default function Footer(){
+    return(
+        <footer className= {styles.footer}>
+            <nav className={styles.nav}>
+                <ul className={styles.ul}>
+                    
+                </ul>
+            </nav>
+        </footer>
+    )
+}
